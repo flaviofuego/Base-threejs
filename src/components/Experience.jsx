89d234b4edf0ed68bbe77d3f483afffdf2bfe1b4
flaviofuego <flavioarregoces@gmail.com>
2024-06-14 14:35:25 -0500

    avanzito

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,33 @@
-import { OrbitControls } from "@react-three/drei";
-
+import {
+  Cylinder,
+  MeshReflectorMaterial,
+  OrbitControls,
+} from "@react-three/drei";
+import { CylinderCollider, RigidBody } from "@react-three/rapier";
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+       <OrbitControls />
+      {/* LIGHTS */}
+      <ambientLight intensity={1} />
+      <directionalLight
+        position={[5, 5, 5]}
+        intensity={0.8}
+        castShadow
+        color={"#9e69da"}
+      />
+      {/*Stage*/}
+      <RigidBody
+          colliders={false}
+          type="fixed"
+          position-y={-0.5}
+          friction={2}
+        >
+          <CylinderCollider args={[1 / 2, 5]} />
+          <Cylinder scale={[5, 1, 5]} receiveShadow>
+            <meshStandardMaterial color="white" />
+          </Cylinder>
+        </RigidBody>
     </>
   );
 };
